74ba4e302eaf450682f9091fe5ccb43929b1afb9
dingning8768 <85069496@qq.com>
2014-02-12 15:39:18 +0800

    Update tab.py

--- tab.py
+++ tab.py
@@ -1,17 +1,8 @@
-# python startup file
+#python startup file 
 import sys
 import readline
 import rlcompleter
 import os
-#import atexit 
-# tab completion
+#tab completion
 readline.parse_and_bind('tab: complete')
-# history file
 histfile = os.path.join(os.environ['HOME'], '.pythonhistory')
-'''
-try:
-    readline.read_history_file(histfile)
-except IOError:
-    pass
-atexit.register(readline.write_history_file, histfile)
-'''
